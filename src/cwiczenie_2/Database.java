cda3a9983ac882819ed695a99c983db9c086164d
MC140180 <140180@student.uwm.edu.pl>
2024-03-10 00:58:13 +0100

    done

--- src/cwiczenie_2/Database.java
+++ src/cwiczenie_2/Database.java
@@ -4,27 +4,45 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Database {
-    static Advertisement GetRandomAd(){
+    static Advertisement GetRandomAd() {
         return new Advertisement("random content");
     }
-    static ArrayList<AdTarget>  getRandomTarget(){
-        // getting target from database
-        // target got element in object with location, if not default message is english
+
+    static ArrayList<AdTarget> getRandomTargets() {
+        // getting targets from database, getting three not just one just for better test, it is mock
+        // target got element in object with location, otherwise default message is in english
+
         ArrayList<AdTarget> targets = new ArrayList<>();
-        AdTarget adTarget =  new AdTarget(new EnglishTranslator(), new PersonalData("andrzej", "andrzej@wp.de", "999888222", "423523-daerg2-aa23-2321", "DE") );
-        AdTarget adTarget1 =  new AdTarget(new EnglishTranslator(), new PersonalData("andrzej", "andrzej@wp.de", "999888222", "423523-daerg2-aa23-2321", "ES") );
-        AdTarget adTarget2 =  new AdTarget(new EnglishTranslator(), new PersonalData("andrzej", "andrzej@wp.de", "999888222", "423523-daerg2-aa23-2321", "EN") );
+        AdTarget adTarget = new AdTarget(new EnglishTranslator(), new PersonalData("andrzej", "andrzej@wp.de", "999888222", "423523-daerg2-aa23-2321", "DE"));
+        AdTarget adTarget1 = new AdTarget(new EnglishTranslator(), new PersonalData("andrzej1", "andrzej1@wp.de", "9299888222", "423523-bbbbb-aa23-2321", "ES"));
+        AdTarget adTarget2 = new AdTarget(new EnglishTranslator(), new PersonalData("andrzej2", "andrzej2@wp.de", "9399888222", "423523-aaaaa-aa23-2321", "EN"));
 
         targets.add(adTarget);
         targets.add(adTarget1);
         targets.add(adTarget2);
 
-        for(AdTarget target:targets){
+        for (AdTarget target : targets) {
             String nationality = target.getPersonalData().getNationality();
             System.out.println(nationality == "EN");
-            if(!(nationality == "EN")) target.setTranslationInterface(TranslationFinder.GetUserTranslationInterface(nationality));
+            if (!(nationality == "EN"))
+                target.setTranslationInterface(TranslationFinder.GetUserTranslationInterface(nationality));
         }
 
-               return targets;
+        return targets;
+    }
+
+    static AdTarget getRandomTarget() {
+        // getting targets from database, getting three not just one just for better test, it is mock
+        // target got element in object with location, otherwise default message is in english
+
+        AdTarget adTarget = new AdTarget(new EnglishTranslator(), new PersonalData("andrzej", "andrzej@wp.de", "999888222", "423523-daerg2-aa23-2321", "DE"));
+
+
+        String nationality = adTarget.getPersonalData().getNationality();
+        System.out.println(nationality == "EN");
+        if (!(nationality == "EN"))
+            adTarget.setTranslationInterface(TranslationFinder.GetUserTranslationInterface(nationality));
+
+        return adTarget;
     }
 }
--- src/cwiczenie_2/Main.java
+++ src/cwiczenie_2/Main.java
@@ -2,10 +2,12 @@ package cwiczenie_2;
 
 import java.util.ArrayList;
 
+
 public class Main {
     public static void main(String[] args) {
        Advertisement advertisement = Database.GetRandomAd();
-        ArrayList<AdTarget> adTargets = Database.getRandomTarget();
+        ArrayList<AdTarget> adTargets = Database.getRandomTargets(); //for better test
+        AdTarget adTarget = Database.getRandomTarget();
 
         Client client1 = new Client("Firma1", new SendByEmail());
         Client client2 = new Client("Firma2", new SendBySMS());
@@ -13,14 +15,15 @@ public class Main {
 
 
         ArrayList<Client> clients = new ArrayList<Client>();
-        clients.add(client2);
         clients.add(client1);
+        clients.add(client2);
         clients.add(client3);
         clients.forEach(client -> {
-            adTargets.forEach(adTarget -> {
-                client.doSendAd(advertisement, adTarget);
+            adTargets.forEach(target -> {
+                client.doSendAd(advertisement, target);
             });
         });
 
+        client1.doSendAd(advertisement, adTarget);
     }
 }
